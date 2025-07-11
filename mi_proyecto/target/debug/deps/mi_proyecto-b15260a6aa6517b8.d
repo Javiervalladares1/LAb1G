624e5a1b339ece805a06a0d141e1615b61699962
Javier Valladares <javivalladares10@icloud.com>
2025-07-11 16:29:25 -0600

    Rust

---
+++ mi_proyecto/target/debug/deps/mi_proyecto-b15260a6aa6517b8.d
@@ -0,0 +1,5 @@
+/Users/javiervalladares/ReactProjects/macshop/LAb1G/mi_proyecto/target/debug/deps/mi_proyecto-b15260a6aa6517b8.d: src/main.rs
+
+/Users/javiervalladares/ReactProjects/macshop/LAb1G/mi_proyecto/target/debug/deps/libmi_proyecto-b15260a6aa6517b8.rmeta: src/main.rs
+
+src/main.rs:
---
+++ mi_proyecto/target/debug/deps/mi_proyecto-d62be26c31762d34.d
@@ -0,0 +1,5 @@
+/Users/javiervalladares/ReactProjects/macshop/LAb1G/mi_proyecto/target/debug/deps/mi_proyecto-d62be26c31762d34.d: src/main.rs
+
+/Users/javiervalladares/ReactProjects/macshop/LAb1G/mi_proyecto/target/debug/deps/libmi_proyecto-d62be26c31762d34.rmeta: src/main.rs
+
+src/main.rs:
